932dc0d53a6d6c09a9d2f4eb1a7ed5189b1c7fa2
AnibalH <robsdatasv@gmail.com>
2023-02-05 13:36:19 -0600

    Refactor exercise 3

--- C1-2023/G2U1/exercise-3/Exercise3/src/main/java/com/mycompany/exercise3/Exercise3.java
+++ C1-2023/G2U1/exercise-3/Exercise3/src/main/java/com/mycompany/exercise3/Exercise3.java
@@ -1,44 +1,40 @@
 
 package com.mycompany.exercise3;
 
-//Escriba un programa que solicite 2 números enteros y pinte las 4 operaciones matemáticas básicas.
+// Escriba un programa que solicite 2 números enteros y pinte las 4 operaciones matemáticas básicas.
 
 import java.util.Scanner;
 
+class Values {
+      Scanner input = new Scanner(System.in);
+      float a = input.nextFloat();
+      float b = input.nextFloat();      
+}
+
 public class Exercise3 {
 
-    public static void main(String[] args) {
-        
-        // Initialize variables
-        int firstDigit;
-        int secondDigit;
-        int sum;
-        int substraction;
-        int multiplication;
-        float division;
-        
-        // Create Scanner object
-        Scanner myObjScanner = new Scanner(System.in);
-        
-        // Ask user for input
-        System.out.println("ENTER FIRST DIGIT: ");
-        firstDigit = myObjScanner.nextInt();
-        System.out.println("ENTER SECOND DIGIT: ");
-        secondDigit = myObjScanner.nextInt();
-        
-        // Do operation
-        sum = firstDigit + secondDigit;
-        substraction = firstDigit - secondDigit;
-        multiplication = firstDigit * secondDigit;
-        division = firstDigit / secondDigit;
-        
+    public static void welcomeMessage(){
+        System.out.println("WELCOME TO THIS BASIC CALCULATOR");
+        System.out.println("ENTER THE DIGITS TO EVALUATE: ");
+    }
+    
+    public static void basicOperations(float a, float b) {
+        float sum = a + b;
+        float substraction = a - b;
+        float multiplication = a * b;
+        float division = a / b;
         // Print results
         System.out.println("RESULTS");
         System.out.println("SUM = " + sum);
         System.out.println("SUBSTRACTION = " + substraction);
         System.out.println("MULTIPLICATION = " + multiplication);
         System.out.println("DIVISION = " + division);
-        
-        
+    }
+    
+    
+    public static void main(String[] args) {
+        welcomeMessage();
+        Values userInput = new Values();
+        basicOperations(userInput.a, userInput.b);
     }
 }
